86fd8a69dc5c62848b7a062b3866a03d823a05ac
AmishaRathore <amisharathore3098@gmail.com>
2020-08-06 11:07:48 +0530

    Updates

---
+++ precache-manifest.b83d274eea3df3149d953a9dfc6f078a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "895ae161bc6dcea972293b9b23e3f1fa",
+    "url": "/Soulip-Internship-Screen-2-react/index.html"
+  },
+  {
+    "revision": "4a372e48cfe4ad4c1a6c",
+    "url": "/Soulip-Internship-Screen-2-react/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "8584176cc7decbc6222a",
+    "url": "/Soulip-Internship-Screen-2-react/static/css/main.aeed885a.chunk.css"
+  },
+  {
+    "revision": "4a372e48cfe4ad4c1a6c",
+    "url": "/Soulip-Internship-Screen-2-react/static/js/2.264c0b89.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Soulip-Internship-Screen-2-react/static/js/2.264c0b89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8584176cc7decbc6222a",
+    "url": "/Soulip-Internship-Screen-2-react/static/js/main.b997fcbd.chunk.js"
+  },
+  {
+    "revision": "efb01eda1a93042c72ec",
+    "url": "/Soulip-Internship-Screen-2-react/static/js/runtime-main.5719dfca.js"
+  }
+]);
